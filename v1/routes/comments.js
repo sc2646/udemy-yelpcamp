2e4925d80a0bf6e147529300c04ecf5dc628f32e
Natalie Chen <natalie.chen@Natalie-Chens-MacBook-Pro.local>
2020-04-05 15:00:56 -0700

    authorization on comments

--- v1/routes/comments.js
+++ v1/routes/comments.js
@@ -44,12 +44,67 @@ router.post("/", isLoggedIn, function(req, res){
 	});
 });
 
+router.get("/:comment_id/edit", checkCommentOnwership, function(req, res){
+	Comment.findById(req.params.comment_id, function(err, foundComment){
+		if(err) {
+			res.redirect("back");
+		} else {
+			res.render("comments/edit", 
+				{
+					campground_id: req.params.id, 
+					comment: foundComment
+				});
+		}
+	});
+});
+
+// COMMENT UPDATE
+router.put("/:comment_id", checkCommentOnwership, function(req, res){
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+			if(err) {
+				res.redirect("back");
+			} else {
+				res.redirect("/campgrounds/" + req.params.id);
+			}
+		});
+});
+
+// COMMENT DESTROY ROUTE
+router.delete("/:comment_id", checkCommentOnwership, function(req, res){
+	Comment.findByIdAndRemove(req.params.comment_id, function(err){
+		if(err){
+			res.redirect("back");
+		}else{
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
 // middleware 
 function isLoggedIn(req, res, next) {
 	if(req.isAuthenticated()){
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCommentOnwership(req, res, next){
+	if(req.isAuthenticated()) {
+		Comment.findById(req.params.comment_id, function(err, foundComment){
+			if(err) {
+				res.redirect("/campgrounds");
+			} else {
+				// does the user own the comment?
+				if(foundComment.author.id.equals(req.user._id)){
+					next();
+				} else {
+					res.redirect("back");
+				}
+			}
+		});
+	} else { // if not, redirect
+		res.redirect("back");
+	}
 }
 
 module.exports = router;
